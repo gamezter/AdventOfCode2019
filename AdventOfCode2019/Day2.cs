afc8b69a5b155e46deeb31e3a122d9761b1dbb9f
Alexandre Charron <alexandre.charron@gameloft.com>
2019-12-03 17:11:23 -0500

    day2 part 1

---
+++ AdventOfCode2019/Day2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2018
+{
+    class Day2
+    {
+        public static void part1()
+        {
+            string[] numbers = new StreamReader("day2.txt").ReadToEnd().Trim().Split(',');
+            int[] prog = new int[numbers.Length];
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                prog[i] = int.Parse(numbers[i]);
+            }
+            prog[1] = 12;
+            prog[2] = 2;
+
+            bool running = true;
+
+            int pc = 0;
+
+            while (running)
+            {
+                int op = prog[pc];
+
+                switch (op)
+                {
+                    case 1:
+                        prog[prog[pc + 3]] = prog[prog[pc + 1]] + prog[prog[pc + 2]];
+                        break;
+                    case 2:
+                        prog[prog[pc + 3]] = prog[prog[pc + 1]] * prog[prog[pc + 2]];
+                        break;
+                    case 99:
+                        running = false;
+                        break;
+                }
+
+                pc += 4;
+            }
+
+
+            Console.WriteLine(prog[0]);
+            Console.Read();
+        }
+
+        public static void part2()
+        {
+        }
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day2.part1();
+        }
+    }
+}
